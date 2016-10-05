6ae904a70caa70da06a8628b430a027b876d504d
Walter Luis <WalterLuis@users.noreply.github.com>
2016-10-05 20:13:35 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,9 +1,9 @@
 SELINUXCONFIGFILE='/etc/selinux/config'
 
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' $SELINUXCONFIGFILE
-sed -i 's/SELINUX=permissive/SELINUX=disabled/g' $SELINUXCONFIGFILE
-setenforce 0
-cat $SELINUXCONFIGFILE | grep '^SELINUX='
+sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' $SELINUXCONFIGFILE
+sudo sed -i 's/SELINUX=permissive/SELINUX=disabled/g' $SELINUXCONFIGFILE
+sudo setenforce 0
+sudo cat $SELINUXCONFIGFILE | grep '^SELINUX='
 
 #setsebool -P httpd_can_network_connect 1
 #setsebool -P httpd_can_network_connect_db 1
@@ -12,31 +12,30 @@ cat $SELINUXCONFIGFILE | grep '^SELINUX='
 #setsebool -P httpd_unified 1
 #setsebool -P polyinstantiation_enabled 1
 
-echo ""
 #Install IUS y EPEL
-yum -q clean all
-yum -q -y install epel-release pwgen git touch 
-yum -q -y upgrade
-wget -q https://centos7.iuscommunity.org/ius-release.rpm
-sudo rpm -q -Uvh ius-release*.rpm
-rm -Rf ius-release.rpm
+sudo yum clean all
+sudo yum -y install epel-release pwgen git touch 
+sudo yum -y upgrade
+sudo wget https://centos7.iuscommunity.org/ius-release.rpm
+sudo sudo rpm -Uvh ius-release*.rpm
+sudo rm -Rf ius-release.rpm
 #Install APACHE
-yum -q -y install httpd
-systemctl start httpd.service
-systemctl enable httpd.service
-firewall-cmd --permanent --add-port=80/tcp
-firewall-cmd --permanent --add-service http
-firewall-cmd --permanent --add-port=443/tcp
-firewall-cmd --permanent --add-service https
-firewall-cmd --permanent --add-port=21/tcp
-firewall-cmd --permanent --add-service=ftp
-firewall-cmd --permanent --add-port=10000/tcp
-systemctl restart firewalld.service
+sudo yum -y install httpd
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-service http
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --permanent --add-service https
+sudo firewall-cmd --permanent --add-port=21/tcp
+sudo firewall-cmd --permanent --add-service=ftp
+sudo firewall-cmd --permanent --add-port=10000/tcp
+sudo systemctl restart firewalld.service
 #Install MySQL
-yum -q -y install mariadb-server mariadb mariadb-libs
-systemctl start mariadb.service
-systemctl enable mariadb.service
-dbpass=$(pwgen -1cnys 24)
+sudo yum -y install mariadb-server mariadb mariadb-libs
+sudo systemctl start mariadb.service
+sudo systemctl enable mariadb.service
+sudo dbpass=$(pwgen -1cnys 24)
 echo ""
 echo "////////////////////////////////////////////////////"
 echo ""
@@ -44,27 +43,27 @@ echo "Password generada de 24bits opcional para password de mysql:  "$dbpass
 echo ""
 echo "////////////////////////////////////////////////////"
 echo ""
-mysql_secure_installation
+sudo mysql_secure_installation
 #Install PHP 5.6
-yum -q -y install php56u php56u-pdo php56u-gd php56u-imap php56u-ldap php56u-xml php56u-intl php56u-soap php56u-mbstring php56u-pear php56u-mysql
-systemctl restart httpd.service
-systemctl enable httpd.service
+sudo yum -y install php56u php56u-pdo php56u-gd php56u-imap php56u-ldap php56u-xml php56u-intl php56u-soap php56u-mbstring php56u-pear php56u-mysql
+sudo systemctl restart httpd.service
+sudo systemctl enable httpd.service
 #Install PEAR library
-pear install Auth_SASL
-pear install Net_SMTP-1.7.2
-pear install Net_IDNA2-0.1.1
-pear install Mail_Mime-1.10.0
-pear install Mail-1.3.0
-pear install Net_URL2-2.2.1
-pear install HTTP_Request2
+sudo pear install Auth_SASL
+sudo pear install Net_SMTP-1.7.2
+sudo pear install Net_IDNA2-0.1.1
+sudo pear install Mail_Mime-1.10.0
+sudo pear install Mail-1.3.0
+sudo pear install Net_URL2-2.2.1
+sudo pear install HTTP_Request2
 #Quitar pagina de bienvenida
-rm -Rf /etc/httpd/conf.d/welcome.conf
-touch /etc/httpd/conf.d/welcome.conf
-echo "#" >> /etc/httpd/conf.d/welcome.conf
+sudo rm -Rf /etc/httpd/conf.d/welcome.conf
+sudo touch /etc/httpd/conf.d/welcome.conf
+sudo echo "#" >> /etc/httpd/conf.d/welcome.conf
 #Configuracion de PHP
-mv -f php.ini /etc/
+sudo mv -f php.ini /etc/
 #Configuracion de MySQL
-mv -f my.cnf /etc/
+sudo mv -f my.cnf /etc/
 #Finalizando instalación
-systemctl restart httpd.service
+sudo systemctl restart httpd.service
 echo "Instalación finalizada"
