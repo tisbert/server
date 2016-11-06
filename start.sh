0d37aa125eaf596c3e23e705e32b77ec097fe23b
Walter Luis <WalterLuis@users.noreply.github.com>
2016-11-06 00:52:21 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -69,8 +69,10 @@ sudo systemctl enable httpd.service
 #Configurando https (pendiente)
 sudo mkdir certificados/
 sudo openssl genrsa -out certificados/CA.key 4096
+echo "localhost"
 sudo openssl req -new -x509 -sha512 -days 1825 -key certificados/CA.key -out certificados/CA.crt
 sudo openssl genrsa -out certificados/IA.key 4096
+echo "localhost.localdomain"
 sudo openssl req -new -sha512 -key certificados/IA.key -out certificados/IA.csr
 sudo openssl x509 -req -sha512 -days 1825 -in certificados/IA.csr -CA certificados/CA.crt -CAkey certificados/CA.key -set_serial 01 -out certificados/IA.crt
 sudo openssl pkcs12 -export -out certificados/IA.p12 -inkey certificados/IA.key -in certificados/IA.crt -chain -CAfile certificados/CA.crt
