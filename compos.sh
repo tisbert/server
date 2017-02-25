616bb366ba6a4469552fcf3cd6a846d7832c3ece
Walter Luis <WalterLuis@users.noreply.github.com>
2017-02-25 12:09:38 +0000

    Update compos.sh

--- compos.sh
+++ compos.sh
@@ -1,11 +1,11 @@
-#sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-#sudo php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-#sudo php composer-setup.php
-#sudo php -r "unlink('composer-setup.php');"
-#sudo rm -Rf composer-setup.php
-#sudo touch composer.json
-#sudo echo "{}" >> composer.json
-#sudo php composer.phar install
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+rm -Rf composer-setup.php
+touch composer.json
+echo "{}" >> composer.json
+php composer.phar install
 
 #Extras - Librerías de composer
 #sudo php composer.phar require zendframework/zendframework
